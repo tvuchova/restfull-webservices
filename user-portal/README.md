52bdc77f497797d4d375096e05557fdf0e44bf9c
Temenuzhka Vuchova <tvuchova@gmail.com>
2018-11-22 07:18:57 +0200

    removed unfinished app user_portal Please enter the commit message for your changes. Lines starting

--- user-portal/README.md
+++
@@ -1,5 +0,0 @@
- # This is educational project ...
-
- Spring boot 2, with JPA, Rest, Liquibase
-
- Start h2 console  to check tables : http://localhost:8080/h2/  with username root and pass : root
--- user-portal/src/main/java/com/example/users/userportal/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.users.userportal.utils;
-
-public class Constants {
-    public static final String GET_USER_BY_ID = "/getUserEntity/{userId}";
-    public static final String GET_ALL_USERS = "/getAllUsers";
-    public static final String SAVE_USER = "/saveUser";
-}
